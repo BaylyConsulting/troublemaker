0994dc472f748628a86ddc9bdb473ac8e1afc0f4
Liam Bayly <ray@baylyconsulting.com>
2018-02-01 14:35:13 -0500

    update code

--- src/home.bloc/home.module.ts
+++ src/home.bloc/home.module.ts
@@ -9,10 +9,12 @@ import { HomeBlocComponent } from './home.component';
 import { HTTPViewComponent } from './views/http-test/index';
 import { IndexViewComponent } from './views/index/index';
 import { VersionViewComponent } from './views/version/index';
+import { ContactViewComponent } from './views/contact/index';
 
 import { SharedModule } from '../shared/shared.module';
 
 // Import Services
+import { CompanyService } from './services/company.service';
 import { ReleaseService } from './services/release.service';
 
 
@@ -20,8 +22,8 @@ import { routing } from './home.routing';
 
 @NgModule({
   imports: [CommonModule, HttpModule, routing, SharedModule, MaterialModule],
-  declarations: [HomeBlocComponent, HTTPViewComponent, IndexViewComponent, VersionViewComponent],
-  providers: [ReleaseService],
+  declarations: [HomeBlocComponent, HTTPViewComponent, IndexViewComponent, VersionViewComponent, ContactViewComponent],
+  providers: [ReleaseService, CompanyService],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeBlocModule {}
